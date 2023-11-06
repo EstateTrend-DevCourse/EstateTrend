d30ea03fc831fe0317d404d1e2c99b00d593ec02
jeremy714 <jeremy714@naver.com>
2023-11-06 21:30:19 +0900

    model 생성

--- trades/views.py
+++ trades/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Test Response")
+
+def sample_url(request):
+    return HttpResponse("Test Response")
