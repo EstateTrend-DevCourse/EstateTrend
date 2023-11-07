4dded90504f7fee8930f9556c8dea6d0e58de97d
Lee Sarah <imsolem1226@gmail.com>
2023-11-07 13:57:45 +0900

    fix: Update view based on model structure
    #8

--- trades/urls.py
+++ trades/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:region_id>/detail/", views.detail, name="detail"),
+]
--- trades/views.py
+++ trades/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test Response")
+    regions = Region.objects.all()
+    context = {"regions": regions}
+    return render(request, "trades/index.html", context)
+
+
+def detail(request, region_id):
+    region = get_object_or_404(Region, pk=region_id)
+    biggest_increase = "정자동"
+    biggest_decrease = "상도동"
+    context = {
+        "region": region,
+        "biggest_increase": biggest_increase,
+        "biggest_decrease": biggest_decrease,
+    }
+    return render(request, "trades/detail.html", context)
