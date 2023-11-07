7dcc0c433d3ddcc2a91f3e300d14439d1e74c886
Lee Sarah <imsolem1226@gmail.com>
2023-11-07 16:41:04 +0900

    feat : Create detail page to see trades
    Use sample data created by admin page.
    main page has a list of region(동)
    and detail pages have a list of real-estates of the region
    and detail-trade pages have a list of trades of the real estate
    Name of classes in View and Templates can be modified.
    #8

--- trades/urls.py
+++ trades/urls.py
@@ -5,5 +5,10 @@ app_name = "trades"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:region_id>/detail/", views.detail, name="detail"),
+    path("detail/<int:region_id>/", views.detail, name="detail"),
+    path(
+        "detail/<int:region_id>/<int:estate_id>/",
+        views.detail_trade,
+        name="detail-trade",
+    ),
 ]
