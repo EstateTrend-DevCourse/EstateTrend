0cc47a64be810613c13401314d91f244b06cbf59
Lee Sarah <imsolem1226@gmail.com>
2023-11-07 10:16:35 +0900

    feat : Add models to admin page
    To create test data at Admin page

--- trades/admin.py
+++ trades/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(RealEstateTrade)
+admin.site.register(RealEstate)
+admin.site.register(Region)
