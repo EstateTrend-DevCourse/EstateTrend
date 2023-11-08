738739a04b7c05f931a81d5721b587908a39d4b9
Lee Sarah <imsolem1226@gmail.com>
2023-11-08 17:26:12 +0900

    chore: Update View for test

--- trades/views.py
+++ trades/views.py
@@ -13,12 +13,26 @@ def index(request):
 
 def detail(request, region_id):
     region = get_object_or_404(Region, pk=region_id)
-
+    estates = region.realestate_set.all()
     biggest_increase = "정자동"
     biggest_decrease = "상도동"
     context = {
         "region": region,
         "biggest_increase": biggest_increase,
         "biggest_decrease": biggest_decrease,
+        "estates": estates,
     }
     return render(request, "trades/detail.html", context)
+
+
+def detail_trade(request, region_id, estate_id):
+    region = get_object_or_404(Region, pk=region_id)
+    estate = get_object_or_404(RealEstate, pk=estate_id)
+
+    real_estate_trades = estate.realestatetrade_set.all()
+    context = {
+        "region": region,
+        "estate": estate,
+        "trades": real_estate_trades,
+    }
+    return render(request, "trades/detail-trade.html", context)
