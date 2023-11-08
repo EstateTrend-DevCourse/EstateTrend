d07e9344a60ee61696872f8dc5c582cc7cad63b0
Lee Sarah <imsolem1226@gmail.com>
2023-11-09 06:54:26 +0900

    fix: Update urls

--- trades/urls.py
+++ trades/urls.py
@@ -6,9 +6,4 @@ app_name = "trades"
 urlpatterns = [
     path("", views.index, name="index"),
     path("detail/<int:region_id>/", views.detail, name="detail"),
-    path(
-        "detail/<int:region_id>/<int:estate_id>/",
-        views.detail_trade,
-        name="detail-trade",
-    ),
 ]
