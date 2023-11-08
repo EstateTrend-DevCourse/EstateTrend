0f341b90edcd0da1e0f6536c1fd4223bf4f93ee9
Lee Sarah <imsolem1226@gmail.com>
2023-11-08 17:20:33 +0900

    feat: Update callapi.py
     that import scraping module
     call data from api & store data into models

--- trades/management/commands/callapi.py
+++ trades/management/commands/callapi.py
@@ -8,18 +8,20 @@ import logging
 
 from django.core.management.base import BaseCommand
 from trades.models import Region, RealEstate, RealEstateTrade
-from trades.views import *
+from utils.scraping import make_dict_result
 
 
 class Command(BaseCommand):
     help = "Call data from api."
 
     def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("Remove data from db"))
         logging.info("Delete data from db")
         Region.objects.all().delete()
         RealEstate.objects.all().delete()
         RealEstateTrade.objects.all().delete()
-
+        self.stdout.write(self.style.SUCCESS("Remove Successed."))
         logging.info("Run python program that call API to get data")
 
+        make_dict_result()
         self.stdout.write(self.style.SUCCESS("Successfully loaded data from API."))
